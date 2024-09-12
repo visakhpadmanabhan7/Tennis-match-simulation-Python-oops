cfc26d58b4acbbc0884953749356541d7bd097e4
Visakh Padmanabhan <visakhpadmanabhan7@gmail.com>
2024-09-13 00:49:46 +0200

    dockeriezd the app

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.11
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+# Run the Python script when the container launches
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -6,20 +6,26 @@ if __name__ == "__main__":
     player1 = Player("player1")
     player2 = Player("player2")
 
+    print('Match 1')
     # First tennis game simulation
     tennis_game1 = TennisGame(player1, player2, input_test=[
         player2, player2, player2, player2, player1, player2
     ])
     tennis_game1.start_match()
 
+    print('----------xxxxxxxxxx------------')
+    print('Match 2')
+
     # Second tennis game simulation
     tennis_game2 = TennisGame(player1, player2, input_test=[
         player2, player2, player1, player1, player1, player1
     ])
     tennis_game2.start_match()
-
+    print('----------xxxxxxxxxx------------')
+    print('Match 3')
     # Third tennis game simulation
     tennis_game3 = TennisGame(player1, player2, input_test=[
         player2, player2, player2, player1, player1, player1, player1, player2, player1, player1
     ])
     tennis_game3.start_match()
+
