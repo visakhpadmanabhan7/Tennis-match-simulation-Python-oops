262fe726abd6c585c708bf8eff22765b1560edbb
Visakh Padmanabhan <visakhpadmanabhan7@gmail.com>
2024-09-12 13:39:04 +0200

    Tennis simulation using python OOPs

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from player import Player
 from tennis_game import TennisGame
 
-player1=Player("player1")
-player2=Player("player2")
-#
-# tennis_game1=TennisGame(player1,player2,input_test=[player2,player2,player2,player2,player1,player2])
-# tennis_game1.start_match()
-#
-# tennis_game1=TennisGame(player1,player2,input_test=[player2,player2,player1,player1,player1,player1])
-# tennis_game1.start_match()
-#
-tennis_game1=TennisGame(player1,player2,input_test=[player2,player2,player2,player1,player1,player1,player1,player2,player1,player1])
-tennis_game1.start_match()
-# #
-# tennis_game1=TennisGame(player1,player2,input_test=[player2,player2,player2,player1,player1,player1,player2,player2])
-# tennis_game1.start_match()
+if __name__ == "__main__":
+    # Create two player objects
+    player1 = Player("player1")
+    player2 = Player("player2")
+
+    # First tennis game simulation
+    tennis_game1 = TennisGame(player1, player2, input_test=[
+        player2, player2, player2, player2, player1, player2
+    ])
+    tennis_game1.start_match()
+
+    # Second tennis game simulation
+    tennis_game2 = TennisGame(player1, player2, input_test=[
+        player2, player2, player1, player1, player1, player1
+    ])
+    tennis_game2.start_match()
+
+    # Third tennis game simulation
+    tennis_game3 = TennisGame(player1, player2, input_test=[
+        player2, player2, player2, player1, player1, player1, player1, player2, player1, player1
+    ])
+    tennis_game3.start_match()
